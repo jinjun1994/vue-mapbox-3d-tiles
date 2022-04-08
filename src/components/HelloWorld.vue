35cd3f05a6005e449c954dd95e7bc32d58820f3d
jinjun1994 <jinjun199403@163.com>
2022-04-08 10:41:44 +0800

    cartographicOrigin.z

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -9,6 +9,7 @@ mapboxgl.accessToken =
 
 const nanXun =
   "https://visom-gis.oss-cn-beijing.aliyuncs.com/3dtiles/%E5%8D%97%E6%B5%94%E6%A8%A1%E5%9E%8B1/%E5%8C%BA%E5%9D%971-%E4%BF%AE%E6%B0%B4%E9%9D%A2-3d%20tiles/Production_2/Scene/Production_2.json";
+const yunYang = "https://yunkaigaosu.cn:1010/data/tilesets/bridge/tileset.json";
 export default {
   data() {
     return {
@@ -26,15 +27,21 @@ export default {
     const nanXunLat = 30.885761605180775;
     const nanXunLng = 120.41632822243005;
 
+    const yunYangAdderss = [108.737581, 30.983239];
+
     const tile3dLayer = new MapboxLayer({
       id: "tile3dlayer",
       type: Tile3DLayer,
       pointSize: 1,
-      data: nanXun,
-      opacity: 0.9,
+      data: yunYang,
+      // data: nanXun,
+      // opacity: 0.9,
       pickable: true,
       loader: Tiles3DLoader,
-      // onTileLoad: (tileHeader) => tileHeader.content.cartographicOrigin.z -= 40,
+      onTileLoad: (tileHeader) => {
+        tileHeader.content.cartographicOrigin.z -= 200; //  z轴偏移
+        console.log(tileHeader)
+        },
       // onTileLoad: (tileHeader) => console.log(tileHeader),
       onHover: (Tile3DLayer, event) =>
         console.log("Hovered:", Tile3DLayer, event),
@@ -42,12 +49,32 @@ export default {
 
     const map = new mapboxgl.Map({
       container: "map", // container ID
-      style: "mapbox://styles/in2twan/ckyzn794b000t14s81978v83n",
-      center: [nanXunLng, nanXunLat],
+      style: 'mapbox://styles/mapbox-map-design/ckhqrf2tz0dt119ny6azh975y',
+      // center: [nanXunLng, nanXunLat],
+      center: yunYangAdderss,
       zoom: zoom,
     });
 
     map.on("load", () => {
+      map.addSource("mapbox-dem", {
+        type: "raster-dem",
+        url: "mapbox://mapbox.mapbox-terrain-dem-v1",
+        tileSize: 512,
+        maxzoom: 14,
+      });
+      // add the DEM source as a terrain layer with exaggerated height
+      map.setTerrain({ source: "mapbox-dem", exaggeration: 1 });
+
+      // add a sky layer that will show when the map is highly pitched
+      map.addLayer({
+        id: "sky",
+        type: "sky",
+        paint: {
+          "sky-type": "atmosphere",
+          "sky-atmosphere-sun": [0.0, 0.0],
+          "sky-atmosphere-sun-intensity": 15,
+        },
+      });
       map.addLayer(tile3dLayer);
       console.log("load");
     });
